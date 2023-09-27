e8c202dd83c0174c4b3a27d9a983b7f5ce7a5001
Morris Kagiri <morriskagiri@gmail.com>
2023-09-27 11:43:10 +0300

    favicon changes

--- server/contact.php
+++ server/contact.php
@@ -18,7 +18,7 @@ if(isset($_POST['email'], $_POST['message'])){
 
     if(!$responseArray){
         $message = $_POST['message'];
-        $headers = 'From: ' . $_POST['email'] . "\r\n" .
+        $headers = 'From: ' . $from . "\r\n" .
             'Reply-To: ' . $_POST['email'] . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
 
@@ -30,34 +30,11 @@ if(isset($_POST['email'], $_POST['message'])){
         }
     }
 }
-try
-{
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
-    $emailText = "You have a new message from your contact form\n=============================\n";
-    foreach ($_POST as $key => $value) {
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-        }
-    }
-    $headers = array('Content-Type: text/plain; charset="UTF-8";',
-        'From: ' . $from,
-        'Reply-To: ' . $_POST['email'],
-        'Return-Path: ' . $from,
-    );
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-}
-catch (\Exception $e)
-{
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
-
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
     $encoded = json_encode($responseArray);
     header('Content-Type: application/json');
     echo $encoded;
-}
-else {
+} else {
     echo $responseArray['message'];
 }
 ?>
